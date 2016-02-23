7bc4ae01f0b442f2c080c5ce3270ab26ced0a1f7
Matthew Sigal <msigal@yorku.ca>
2016-02-23 16:15:56 -0500

    Initial commit.

---
+++ R/SimDisplay-package.R
@@ -0,0 +1,15 @@
+# SimDisplay-package
+#'
+#' The SimDisplay package provides a variety of methods for visualizing
+#' results from complex tables. It has been specifically designed to
+#' summarise Monte Carlo Simulation Study objects from SimDesign (Chalmers, 2016).
+#'
+#' @section SimDisplay functions:
+#' So far, this package implements the following functions: \code{tableplot}.
+#'
+#' @name SimDisplay-package
+#' @title SimDisplay - Methods for visualizing complex tables.
+#' @author Matthew Sigal \email{matthewsigal@@gmail.com}
+#' @docType package
+#' @keywords package
+NULL

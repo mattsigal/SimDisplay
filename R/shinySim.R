c910b6a510afc32afabcca097831fa0418cb6d8d
Matthew Sigal <msigal@yorku.ca>
2016-02-25 10:17:54 -0500

    Provide sample simulation results, update shinySim - got it working!

--- R/shinySim.R
+++ R/shinySim.R
@@ -9,62 +9,57 @@
 #'
 #' @examples
 #' \dontrun{
+#' data(tsimresults)
+#' str(tsimresults)
+#' head(tsimresults)
+#' runApp(shinySim(tsimresults))
 #' }
 #'
 #' @seealso \code{\link{SimDisplay}}
 shinySim <- function(x){
-
+  library(shiny)
   library(ggplot2)
-
-  # Convert datafile to simDat structure
+  # Check datafile for simDat structure
   simDat <- sim_design_check(x)
 
   ret <- list(
 
-    ui = shiny::pageWithSidebar(
-
+    ui = pageWithSidebar(
       # Application title
-      shiny::headerPanel("Simulation Display"),
-
-      shiny::sidebarPanel(
-
-        shiny::h5('Use the following to produce your display'),
-
-        shiny::selectInput(inputId = "rowvar",
+      headerPanel("Simulation Display"),
+      #-----------------------------------------------------------------------
+      sidebarPanel(
+        h5('Use the following to produce your display'),
+        selectInput(inputId = "rowvar",
                            label = "Row variable:",
                            choices = get_design_levels(x),
                            selected = get_design_levels(x)[1]),
-
-        shiny::selectInput(inputId = "colvar",
+        selectInput(inputId = "colvar",
                            label = "Column Variable:",
                            choices = get_design_levels(x),
                            selected = get_design_levels(x)[2]),
-
-        shiny::selectInput(inputId = "facetvar",
+        selectInput(inputId = "facetvar",
                            label = "Facet Table?",
-                           choices = c(No = "No",
-                                       Yes = "Yes"),
+                           choices = c("No" = "No",
+                                       "Yes" = "Yes"),
                            selected = "No"),
-
-        shiny::conditionalPanel(condition = "facetvar == 'Yes'",
-                                shiny::selectInput(inputId = "facvar",
-                                                   label = "Facet Variable:",
-                                                   choices = get_design_levels(x),
-                                                   selected = get_design_levels(x)[3])),
-
-        shiny::selectInput(inputId = "responsevar",
+        conditionalPanel(condition = 'input.facetvar == "Yes"',
+                         selectInput(inputId = "facvar",
+                                     label = "Facet Variable:",
+                                     choices = get_design_levels(x),
+                                     selected = get_design_levels(x)[3])),
+        selectInput(inputId = "responsevar",
                            label = "Response Variable:",
                            choices = get_sim_levels(x),
                            selected = get_sim_levels(x)[1])
         ),
         #-----------------------------------------------------------------------
-        shiny::mainPanel(
-          shiny::plotOutput(outputId = "main_plot", height = "700px", width = "700px"),
-          shiny::textOutput(outputId = "facet_status")
+        mainPanel(
+          plotOutput(outputId = "main_plot", height = "700px", width = "700px"),
+          textOutput(outputId = "facet_status")
         )),
-
+#-----------------------------------------------------------------------
     server = function(input, output) {
-
       getdat <- function(input){
         if(input$facetvar == "No") {
           dat <- simDat[,c(input$rowvar, input$colvar, input$responsevar)]
@@ -75,22 +70,17 @@ shinySim <- function(x){
         return(dat)
       }
 
-      output$facet_status <- shiny::renderPrint(input$facetvar)
+      output$facet_status <- renderPrint(c(names(getdat(input)), input$facetvar, input$rowvar, input$colvar, input$responsevar))
 
-      output$main_plot <- shiny::renderPlot({
-        dat <- getdat(input)
-        p <- ggplot(data = dat,
-                    aes(y=dat[,1],
-                        x=dat[,2])) +
-          geom_raster(aes(fill=dat[,3])) +
-          labs(x = input$colvar,
-               y = input$rowvar)
-
-        print(p)
-
-        if (input$facetvar == "Yes") {
-          p + facet_grid(paste(". ~ ", colnames(dat,4)))
-        }
+      output$main_plot <- renderPlot({
+          dat <- getdat(input)
+          p <- ggplot(data = dat, aes_string(y=input$colvar, x=input$rowvar)) +
+            geom_raster(aes_string(fill=input$responsevar)) +
+            labs(x = input$colvar, y = input$rowvar)
+          print(p)
+          if (input$facetvar == "Yes") {
+            p + facet_grid(paste(". ~ ", input$facvar))
+          }
         })
     }
   )

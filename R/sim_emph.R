5cd5aa987b4c1a6c36f438fa98fe6a2727a07dec
Matthew Sigal <matthewsigal@gmail.com>
2017-06-15 13:10:53 -0400

    First pass at sim_emph().

---
+++ R/sim_emph.R
@@ -0,0 +1,30 @@
+#' Emphasize cells from simulation results
+#'
+#' \code{sim_emph} takes a \code{SimDesign} dataframe object, and returns a
+#'
+#' @param dat A \code{data.frame} object of \code{class(SimDesign)}.
+#' @param upper.bound An \code{integer} value indicating the upper limit.
+#' @param lower.bound An \code{integer} value indicating the lower limit.
+#'
+#' @return A character \code{vector}.
+#'
+#' @seealso \code{\link{SimDisplay}}
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' data(Brown1974)
+#' Brown1974$REPLICATIONS <- Brown1974$SIM_TIME <- Brown1974$COMPLETED <- Brown1974$SEED <- NULL
+#' TypeI <- subset(Brown1974, var_ratio == 1) # Separate type I error rate conditions
+#' Power <- subset(Brown1974, var_ratio != 1) # from power conditions
+#' cat(sim_emph(TypeI), sep = '\n')
+#' }
+#'
+sim_emph <- function(dat, upper.bound = .075, lower.bound = .025){
+  require(pander)
+  panderOptions('table.split.table', Inf)
+  panderOptions('table.style', 'simple')
+  emph <- suppressWarnings(which(out > upper.bound | out < lower.bound, arr.ind = TRUE))
+  ret <- pander_return(out, round = 2, emphasize.strong.cells = emph)
+  return(ret)
+}

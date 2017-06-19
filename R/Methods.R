4f1f3ee316e9ffc93a3b34923b66cde2604a6398
Matthew Sigal <matthewsigal@gmail.com>
2017-06-19 12:47:35 -0400

    Typo.

--- R/Methods.R
+++ R/Methods.R
@@ -15,6 +15,7 @@
 #'
 #' @examples
 #' \dontrun{
+#' data(Brown1974)
 #' summary(Brown1974)
 #' type1 <- subset(Brown1974, var_ratio == 1)
 #' power <- subset(Brown1974, var_ratio != 1)
@@ -27,18 +28,19 @@ summary.SimDesign <- function(object, digits = 3, percent = FALSE, ...) {
   if (!("SimDesign" %in% class(object)))
     stop("This object is not of class SimDesign. See convert_df().")
 
-  groupColumns <- SimDisplay:::get_design_levels(object)
-  dataColumns <- SimDisplay:::get_sim_levels(object)
+  groupColumns <- get_design_levels(object)
+  dataColumns <- get_sim_levels(object)
 
   out <- vector('list', length(groupColumns))
   for (i in 1:length(groupColumns)){
-    res <- ddply(x, groupColumns[i],
-                 function(x) roundSim(colMeans(x[dataColumns]),
-                                       digits = digits,
-                                       percent = percent))
+    res <- plyr::ddply(object, groupColumns[i],
+                       function(x) roundSim(colMeans(object[dataColumns]),
+                                             digits = digits,
+                                             percent = percent))
     out[[i]] <- res
   }
   names(out) <- groupColumns
   return(out)
 }
+
 

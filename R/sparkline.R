eb942e188e9f844f271dc8ad3819e2556e7b0cd8
Matthew Sigal <matthewsigal@gmail.com>
2017-06-20 10:49:53 -0400

    Add min/max arguments to sparkline.

--- R/sparkline.R
+++ R/sparkline.R
@@ -3,6 +3,8 @@
 #' \code{sparkline} takes a vector of type numeric and generates a simple sparkline graphic.
 #'
 #' @param x A \code{vector} object, pertaining to a row from a tabular display.
+#' @param min Numeric input, specifying the minimum value for the y-axis
+#' @param max Numeric input, specifying the maximum value for the y-axis
 #'
 #' @return A \code{ggplot2} graphic object.
 #'
@@ -12,14 +14,18 @@
 #' @examples
 #' \dontrun{
 #' data(Brown1974)
+#' sparkline(Brown1974[1,4:9])
+#' sparkline(Brown1974[64,4:9])
 #' }
 #'
-sparkline <- function(x){
+sparkline <- function(x, min = 0, max = 1){
+
   pdat <- data.frame(Value = as.numeric(x),
                      Method = factor(seq(1, length(as.numeric(x)))))
   plot <- ggplot(pdat, aes(y = Value,
                            x = Method,
                            group = 1)) +
+    scale_y_continuous(limits = c(min, max)) +
     geom_line(size = 2) + theme_spark()
   out <- ggplotGrob(plot)
   matches <- grepl("panel", out$layout$name)

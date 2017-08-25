fe44e019cd4ae02686b7ee702cacb7375698a70a
Matthew Sigal <matthewsigal@gmail.com>
2017-08-25 15:30:35 -0400

    Initial attempts at data filtering.

--- R/shinyMCSS.R
+++ R/shinyMCSS.R
@@ -32,6 +32,7 @@
 
 shinyMCSS <- function(filename = NULL, export = FALSE){
   library(shiny)
+  library(shinydashboard)
 
   # Load example dataset or passed dataframe:
   if (is.null(filename)) {
@@ -44,30 +45,107 @@ shinyMCSS <- function(filename = NULL, export = FALSE){
   } else dat <- filename
 
   ## UI #############################
-  ui = fluidPage(
-    checkboxGroupInput(inputId = "design",
-                   label = "Design Variables:",
-                   choices = attributes(dat)$design_names$design,
-                   selected = attributes(dat)$design_names$design),
-    checkboxGroupInput(inputId = "response",
-                   label = "Response Variables:",
-                   choices = attributes(dat)$design_names$sim,
-                   selected = attributes(dat)$design_names$sim),
-    checkboxGroupInput(inputId = "meta",
-                       label = "Meta Variables:",
-                       choices = attributes(dat)$design_names$extra,
-                       selected = attributes(dat)$design_names$extra),
-    tableOutput("data")
-  )
+  ui = dashboardPage(
+    dashboardHeader(title = "Shiny SimDisplay"),
+    skin = "black",
+
+    # DASHBOARD SIDEBAR
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Initialization", tabName = "init", icon = icon("dashboard")),
+        menuItem("Models", tabName = "models", icon = icon("table")),
+        menuItem("Visualizations", tabName = "dataviz", icon = icon("line-chart"))
+      )
+    ),
+
+    # DASHBOARD BODY
+    dashboardBody(
+      tabItems(
+        # INITIALIZATION TAB
+        tabItem(tabName = "init",
+                fluidRow(
+                  # Checkbox inputs for design, response, and meta variables:
+                  box(title="Design Variables and Filters",
+                      width = 4, solidheader = TRUE, status = "primary",
+                      checkboxGroupInput(inputId = "design",
+                                     label = NULL,
+                                     choices = attributes(dat)$design_names$design,
+                                     selected = attributes(dat)$design_names$design),
+                      uiOutput("filters")),
+                  box(title = "Response Variables:",
+                      width = 4, solidheader = TRUE, status = "primary",
+                      checkboxGroupInput(inputId = "response",
+                                         label = NULL,
+                                         choices = attributes(dat)$design_names$sim,
+                                     selected = attributes(dat)$design_names$sim)),
+                  box(title = "Meta Variables:",
+                      width = 4, solidheader = TRUE, status = "warning",
+                      checkboxGroupInput(inputId = "meta",
+                                     label = NULL,
+                                     choices = attributes(dat)$design_names$extra,
+                                     selected = NULL)),
+                  dataTableOutput("data")
+                )),
+        # MODELS TAB
+        tabItem(tabName = "models",
+                h2("Models")),
+        # DATA VIZ TAB
+        tabItem(tabName = "dataviz",
+                h2("Data Visualizations"))
+      ) # end of tabitems
+    ) # end of dashboard body
+  ) # end of dashboard page
 
   ## SERVER #########################
   server = function(input, output, session) {
-    output$data <- renderTable({
-      dat[, c(input$design, input$response, input$meta), drop = FALSE]
-    }, rownames = TRUE)
+
+    # SUBSET COLUMNS BASED UPON INPUTS:
+    dat_subset <- reactive({
+      cols <- subset(dat,
+                     select = c(input$design, input$response, input$meta),
+                     drop = FALSE)
+      # THIS SHOULD ALSO UTILIZE THE ROW FILTERS THAT ARE GENERATED BELOW!!!
+
+      for (i in 1:length(input$design)){
+        var <- input$design[[i]]
+        if (!is.null(input[[var]])){
+          print(input[[var]]) # returns the levels of the design factor
+          keep_rows <- input[[var]] %in% levels(dat[[var]])
+
+        }
+      }
+
+      return(cols)
+    })
+
+    # RENDER FILTERS FOR DESIGN VARIABLES:
+    first_run <<- TRUE # FLAG FOR SCRIPT INITIALIZATION
+    output$filters <- renderUI({
+      if (first_run){
+        num_filters <- isolate(length(input$design))
+        filters <- list()
+        for (i in 1:num_filters){
+          filt <- input$design[[i]]
+          filters[[i]] <- checkboxGroupInput(inputId = filt,
+                                             label = filt,
+                                             choices = levels(dat_subset()[[filt]]),
+                                             selected = levels(dat_subset()[[filt]]))
+        }
+        filters_save <<- filters
+        first_run <<- FALSE
+        filters
+      } else filters_save
+    })
+
+    # RENDER DATA TABLE:
+    output$data <- renderDataTable({
+      dat_subset()
+    }, options = list(paging = FALSE,
+                      dom  = '<"top">lrt<"bottom">ip'))
   }
 
+  # RUN THE SHINY APP:
   runApp(list(ui = ui, server = server))
 }
 
-# shinyMCSS()
+#shinyMCSS()
